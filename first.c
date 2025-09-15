25c5398f8c3ba890309225fd46e25b8322574061
kinza-khan <kinzaaptech@gmail.com>
2025-09-15 10:44:46 +0500

    conditional_statements

--- first.c
+++ first.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+main(){
+char qualification[10];
+char result[10];
+printf("Enter Your Qualification ");
+scanf("%s",qualification);
+if(strcmp(qualification,"MS") == 0){
+		printf("Enter Your Result ");
+		scanf("%s",result);
+		if(strcmp(result,"Pass") == 0){
+				printf("Congratulations You are hired");
+		}
+		else{
+			printf("Better luck next time");
+		}
+}	
+else{
+	printf("Not Eligible");	
+}
+}
